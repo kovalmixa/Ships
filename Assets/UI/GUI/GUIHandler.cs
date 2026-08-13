e7515bbaebee766ced9839afd0c5cb312a92d2ae
unknown <kovalchukmixa@gmail.com>
2026-08-13 10:50:01 +0300

    started adding command line and gui

---
+++ Assets/UI/GUI/GUIHandler.cs
@@ -0,0 +1,14 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GUIHandler : SingletonMonoBehaviour<GUIHandler>
+{
+    [SerializeField] private CommandLine commandLine;
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode./))
+        {
+            //targetButton.onClick.Invoke();
+        }
+    }
+}
